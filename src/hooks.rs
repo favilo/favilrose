76c0d432111c49d0eb021ecd6ea211ec68486dd8
favilo <kevin.oberlies@elastic.co>
2024-03-28 14:11:52 -0700

    Create Zoom class to check for floating zoom windows.
    https://www.peterstuart.org/posts/2021-09-06-xmonad-zoom/

--- src/hooks.rs
+++ src/hooks.rs
@@ -3,20 +3,65 @@ use penrose::{
         hooks::{ManageHook, StateHook},
         State,
     },
-    extensions::hooks::manage::{FloatingFixed, SetWorkspace},
+    extensions::hooks::manage::{DefaultTiled, FloatingFixed, SetWorkspace},
     manage_hooks,
     pure::geometry::Rect,
     x::{
-        query::{ClassName, StringProperty},
+        query::{ClassName, StringProperty, Title},
         Atom, Query, XConn,
     },
 };
 
 use crate::BAR_HEIGHT_PX;
 
+const ZOOM_TILE_TITLES: [&str; 4] = [
+    "Zoom - Free Account",     // main window
+    "Zoom - Licensed Account", // main window
+    "Zoom",                    // meeting window on creation
+    "Zoom Meeting",            // meeting window shortly after creation
+];
+
+struct ZoomTiledQuery;
+
+impl<X: XConn> Query<X> for ZoomTiledQuery {
+    fn run(&self, id: penrose::Xid, x: &X) -> penrose::Result<bool> {
+        let zoom_class = ClassName("zoom").run(id, x)?;
+        if !zoom_class {
+            return Ok(false);
+        }
+
+        for title in ZOOM_TILE_TITLES.iter() {
+            if Title(title).run(id, x)? {
+                return Ok(true);
+            }
+        }
+        Ok(false)
+    }
+}
+
+struct ZoomFloatQuery;
+
+impl<X: XConn> Query<X> for ZoomFloatQuery {
+    fn run(&self, id: penrose::Xid, x: &X) -> penrose::Result<bool> {
+        let zoom_class = ClassName("zoom").run(id, x)?;
+        if !zoom_class {
+            return Ok(false);
+        }
+
+        for title in ZOOM_TILE_TITLES.iter() {
+            if Title(title).run(id, x)? {
+                return Ok(false);
+            }
+        }
+        Ok(true)
+    }
+}
+
 pub fn manage_hook<'a, X: XConn + 'static>() -> Box<dyn ManageHook<X> + 'a> {
     manage_hooks! {
         // ClassName("discord") => SetWorkspace("3"),
+        ZoomTiledQuery => DefaultTiled,
+        ZoomFloatQuery => FloatingFixed(Rect::new(0, 0, 200, 100)),
         IsDock => FloatingFixed(Rect::new(0, 0, 100, BAR_HEIGHT_PX)).then(IgnoreWindow),
         ClassName("stalonetray") => FloatingFixed(Rect::new(0, 0, 100, BAR_HEIGHT_PX)),
     }
